6ec8c5e17cc8ffe7f67157d6b63d7517d29b2dd2
Sushant Nariangadu <nsushant2007@gmail.com>
2026-02-04 02:00:01 +0530

    Made all installables indexable.
    Does hack so spotlight and raycast can index and search

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -9,7 +9,7 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
-    configuration = { pkgs, ... }: {
+    configuration = { pkgs, config, ... }: {
       nixpkgs.config.allowUnfree = true;
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
@@ -26,6 +26,7 @@
           pkgs.spotify
           pkgs.docker
 
+          pkgs.mkalias
           pkgs.neovim
           pkgs.zellij
           pkgs.dust
@@ -58,6 +59,28 @@
         pkgs.lilex 
         pkgs.nerd-fonts.lilex 
       ];
+
+      
+system.activationScripts.applications.text = let
+  env = pkgs.buildEnv {
+    name = "system-applications";
+    paths = config.environment.systemPackages;
+    pathsToLink = ["/Applications"];
+  };
+in
+  pkgs.lib.mkForce ''
+  # Set up applications.
+  echo "setting up /Applications..." >&2
+  rm -rf /Applications/Nix\ Apps
+  mkdir -p /Applications/Nix\ Apps
+  find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+  while read -r src; do
+    app_name=$(basename "$src")
+    echo "copying $src" >&2
+    ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+  done
+      '';
+
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
