4cc191bfa3be7814b099c30064cd096faaaeae94
Sushant Nariangadu <nsushant2007@gmail.com>
2026-02-21 22:12:06 +0530

    Switch to LazyVim adding nvim config to github

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4        -- number of visual spaces per TAB
+vim.opt.shiftwidth = 4     -- spaces used for autoindent
+vim.opt.expandtab = true   -- convert tabs to spaces
+vim.opt.smartindent = true -- smarter autoindent
+
+vim.opt.autoread = true
