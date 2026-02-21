f0e8f442607585db60e6a08a9a005ee5142301d5
Sushant Nariangadu <nsushant2007@gmail.com>
2026-02-21 22:35:28 +0530

    Set lazy

--- .config/nvim/lua/plugins/initial.lua
+++ .config/nvim/lua/plugins/initial.lua
@@ -13,47 +13,23 @@ return {
   },
 
 --   -- change trouble config
---   {
---     "folke/trouble.nvim",
---     -- opts will be merged with the parent spec
---     opts = { use_diagnostic_signs = true },
---   },
+  {
+    "folke/trouble.nvim",
+    -- opts will be merged with the parent spec
+    opts = { use_diagnostic_signs = true },
+  },
 
---   -- disable trouble
---   { "folke/trouble.nvim", enabled = false },
 
 --   -- override nvim-cmp and add cmp-emoji
---   {
---     "hrsh7th/nvim-cmp",
---     dependencies = { "hrsh7th/cmp-emoji" },
---     ---@param opts cmp.ConfigSchema
---     opts = function(_, opts)
---       table.insert(opts.sources, { name = "emoji" })
---     end,
---   },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "emoji" })
+    end,
+  },
 
---   -- change some telescope options and a keymap to browse plugin files
---   {
---     "nvim-telescope/telescope.nvim",
---     keys = {
---       -- add a keymap to browse plugin files
---       -- stylua: ignore
---       {
---         "<leader>fp",
---         function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
---         desc = "Find Plugin File",
---       },
---     },
---     -- change some options
---     opts = {
---       defaults = {
---         layout_strategy = "horizontal",
---         layout_config = { prompt_position = "top" },
---         sorting_strategy = "ascending",
---         winblend = 0,
---       },
---     },
---   },
 
 --   -- add pyright to lspconfig
 --   {
@@ -108,41 +84,32 @@ return {
 --   { import = "lazyvim.plugins.extras.lang.typescript" },
 
 --   -- add more treesitter parsers
---   {
---     "nvim-treesitter/nvim-treesitter",
---     opts = {
---       ensure_installed = {
---         "bash",
---         "html",
---         "javascript",
---         "json",
---         "lua",
---         "markdown",
---         "markdown_inline",
---         "python",
---         "query",
---         "regex",
---         "tsx",
---         "typescript",
---         "vim",
---         "yaml",
---       },
---     },
---   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "json",
+        "yaml",
+        "xml",
+        "toml",
+        "sql",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "regex",
+        "rust",
+        "go",
+        "vim",
+      },
+    },
+  },
 
---   -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
---   -- would overwrite `ensure_installed` with the new value.
---   -- If you'd rather extend the default config, use the code below instead:
---   {
---     "nvim-treesitter/nvim-treesitter",
---     opts = function(_, opts)
---       -- add tsx and treesitter
---       vim.list_extend(opts.ensure_installed, {
---         "tsx",
---         "typescript",
---       })
---     end,
---   },
 
 --   -- the opts function can also be used to change the default opts:
 --   {
@@ -175,15 +142,23 @@ return {
 --   { import = "lazyvim.plugins.extras.lang.json" },
 
 --   -- add any tools you want to have installed below
---   {
---     "williamboman/mason.nvim",
---     opts = {
---       ensure_installed = {
---         "stylua",
---         "shellcheck",
---         "shfmt",
---         "flake8",
---       },
---     },
---   },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "prettier",
+        "eslint_d",
+        "rust-analyzer",
+        "gopls",
+        "taplo",
+        "yaml-language-server",
+        "json-lsp",
+        "html-lsp",
+        "css-lsp",
+        "typescript-language-server",
+        "sqls",
+      },
+    },
+  },
 }
